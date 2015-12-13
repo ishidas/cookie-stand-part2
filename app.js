e69ca124d47d60eb20fbed5048964a5cd9f155fc
Sawako Ishida <sawako.yoriko@gmail.com>
2015-12-13 00:07:45 -0800

    Added hourlycookies array and fixed bug on random num

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var allStores = [];
 var totalCookies = [];
 var hoursOfOp = ['Store Name','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
-// var trow = document.createElement('tr');
-// var tdata = document.createElement('td');
 
 //store object
 function Store(storeName,maxCustmrPerHr,minCustmrPerHr,aveCookiePerHr) {
@@ -10,79 +8,47 @@ function Store(storeName,maxCustmrPerHr,minCustmrPerHr,aveCookiePerHr) {
   this.maxCustmrPerHr = maxCustmrPerHr;
   this.minCustmrPerHr = minCustmrPerHr;
   this.aveCookiePerHr = aveCookiePerHr;
+  this.hourlyCookies = [];
+  allStores.push(this);
+
 }
+
+Store.prototype.randomNum = function () {
+  return Math.floor(Math.random() * (this.maxCustmrPerHr - this.minCustmrPerHr + 1)) + this.minCustmrPerHr;
+
+};
+
+//Hourlycookies Array data creating function
+Store.prototype.totalCookiesPerHr = function() {
+  var hrCookies1 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies2 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies3 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies4 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies5 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies6 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies7 = Math.round(this.aveCookiePerHr * this.randomNum());
+  var hrCookies8 = Math.round(this.aveCookiePerHr * this.randomNum());
+  return this.hourlyCookies.push(hrCookies1,hrCookies2,hrCookies3,hrCookies4,hrCookies5,hrCookies6,hrCookies7,hrCookies8);
+  console.log(totalCookiesPerHr());
+};
+
+// Store.prototype.hourlyCookiesArray = function() {
+//     this.hourlyCookies.push(this.totalCookiesPerHr());
+// };
 
 //new stores
-function storesCreated() {
-  var PP = new Store('Pike Place Market','88','17','5.2');
-  var STA = new Store('Sea Tac Airport','44','6','1.2');
-  var SCM = new Store('Southcenter Mall','38','11','1.9');
-  var BS = new Store('Bellevue Square Mall','48','20','3.3');
-  var ALK = new Store('Alki','24','3','2.6');
-  allStores.push(PP,STA,SCM,BS,ALK);
-} storesCreated();
+var PP = new Store('Pike Place Market',88,17,5.2);
+var STA = new Store('Sea Tac Airport',44,6,1.2);
+var SCM = new Store('Southcenter Mall',38,11,1.9);
+var BS = new Store('Bellevue Square Mall',48,20,3.3);
+var ALK = new Store('Alki',24,3,2.6);
 
-//random number generator
-function randomNum() {
-  for( var i = 0; i < allStores.length; i++ ){
-    var maxCust = parseInt(allStores[i].maxCustmrPerHr);
-    var minCust = parseInt(allStores[i].minCustmrPerHr);
-    return Math.floor(Math.random() * (maxCust- minCust)) + minCust
-  }
-}
-var randomNumOfCust = randomNum();
-
-//Total Cookies per Hour
-function totalCookiesPerHr() {
-  for( var j = 1; j < hoursOfOp.length; j++ ){
-    for( var i = 0; i < allStores.length; i++ ){
-      var aveCookies = parseInt(allStores[i].aveCookiePerHr);
-      var totalC = randomNumOfCust * aveCookies;
-    }
-      randomNumOfCust = randomNum();
-      totalCookies.push(totalC);
-
-  }
-} totalCookiesPerHr()
-
-//Creating Table
-  var tableEl = document.getElementById('salestable');
-  var thead = document.createElement('th');
-  var trow = document.createElement('tr');
-//creating Table head
-function createTableHead() {
-  for( var i = 0; i < hoursOfOp.length; i++ ){
-    var tdata = document.createElement('td');
-    tdata.textContent = hoursOfOp[i];
-    trow.appendChild(tdata);
-  }
-    thead.appendChild(trow);
-    tableEl.appendChild(thead);
-} createTableHead();
-
-// Creating Table rows with sales data and store name
-function createTableRows() {
-    for( var i = 0; i < allStores.length; i++ ){
-      var trow2 = document.createElement('tr');
-      trow2.setAttribute('id','row'+[i]);
-      var tdata2 = document.createElement('td');
-      tdata2.textContent = allStores[i].storeName;
-      trow2.appendChild(tdata2);
-      tableEl.appendChild(trow2);
-
-    for( var j = 0; j < hoursOfOp.length; j++ ){
-    var rowEl = document.getElementById('row'+[i]);
-    var tdata3 = document.createElement('td');
-    tdata3.textContent = totalCookies[j];
-    rowEl.appendChild(tdata3);
-    tableEl.appendChild(rowEl);
-    // break;
-    }
-  }
-} createTableRows();
-
-
-
+//Total Cookies Per Hr Array
+PP.totalCookiesPerHr();
+STA.totalCookiesPerHr();
+SCM.totalCookiesPerHr();
+BS.totalCookiesPerHr();
+ALK.totalCookiesPerHr();
 
 
 
